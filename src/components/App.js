f0c04751c98175235d9ad8fd14910cd60b106ee9
cjl72 <55719709+cjl72@users.noreply.github.com>
2021-04-03 17:08:07 -0400

    Task: Edits to TaskBoard and App

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-
 import TaskBoard from './TaskBoard';
 import Task from './Task';
 import PageTabs  from './PageTabs';
@@ -9,12 +8,36 @@ import InProgress from './InProgress';
 import Review from './Review';
 import Done from './Done';
 
+const LARGE_DESKTOP_BREAKPOINT = 1366;
+const SMALL_DESKTOP_BREAKPOINT = 1024;
+const TABLET_BREAKPOINT = 768;
+
 class App extends React.Component {
     state = {
         view: 'task board',
-        windowWidth: window.innerWidth
+        browserWidth: 0,
+        breakpoint: 'large-desktop'
     };
 
+    componentDidMount() {
+        window.addEventListener('resize', this.handleResize);
+        this.handleResize();
+    }
+
+    handleResize = () => {
+        const browserWidth = window.innerWidth;
+        let breakpoint = 'large-desktop';
+
+        if (browserWidth < LARGE_DESKTOP_BREAKPOINT && browserWidth >= SMALL_DESKTOP_BREAKPOINT) {
+            breakpoint = 'small-desktop';
+        } else if (browserWidth < SMALL_DESKTOP_BREAKPOINT && browserWidth >= TABLET_BREAKPOINT) {
+            breakpoint = 'tablet';
+        } else if (browserWidth < TABLET_BREAKPOINT) {
+            breakpoint = 'mobile';
+        }
+
+        this.setState({ breakpoint, browserWidth });
+    }
 
     onViewChange(view) {
         this.setState({ view });
@@ -32,29 +55,26 @@ class App extends React.Component {
     }
 
     render() {
-        const { view } = this.state;
+        const { breakpoint } = this.state;
 
-        switch (view) {
-            case 'task board' :
+        switch (breakpoint) {
+            case 'large-desktop' :
                 return (this.wrapPage(
                     <TaskBoard />
                 ));
-            case 'todo':
+            case 'small-desktop':
+                return (this.wrapPage(
+                    <TaskBoard />
+                ));
+            case 'tablet':
+                return (this.wrapPage(
+                    <TaskBoard />
+                ));
+            case 'mobile':
                 return (this.wrapPage(
                     <ToDo />
                 ));
-            case 'inprogress':
-                return (this.wrapPage(
-                    <InProgress />
-                ));
-            case 'review':
-                return (this.wrapPage(
-                    <Review />
-                ));
-            case 'done':
-                return (this.wrapPage(
-                    <Done />
-                ));
+
         }
         // No Default
     }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 class ToDo extends React.Component {
     constructor(props) {
         super(props);
